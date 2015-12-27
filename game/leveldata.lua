c70ca8bbc2e5db98447235a6da22d57a320bcf9c
Ralph Ward <ralph.ward@gmail.com>
2015-12-27 21:30:34 +1100

    Made survival mode more random

--- game/leveldata.lua
+++ game/leveldata.lua
@@ -10,6 +10,7 @@ end
 
 function M:getLevel(levelNum)
 
+	print ("Level Number: " .. levelNum)
 	if levelNum == -1 then
 		return M:getLevelEasy()		
 	elseif levelNum == -2 then
@@ -17,8 +18,9 @@ function M:getLevel(levelNum)
 	elseif levelNum == 1 then
 		return M:getLevel1()
 	else
-		return M:getLevelEasy()
+		return M:getLevel1()
 	end
+
 	return {}
 end
 
@@ -39,8 +41,8 @@ end
 
 function M:getLevelEasy()
 	local E = {}
-	E[1] = getStandardEnemy(500, 75, "a", 1)
-	E[2] = getStandardEnemy(1000, 350, "b", 1)
+	E[1] = getStandardEnemy(1500, 75, "a", 0)
+	E[2] = getStandardEnemy(1000, 350, "b", 0)
 
 	return E
 
@@ -49,13 +51,13 @@ end
 function M:getLevelHard()
 	local E = {}
 
-	E[1] = getStandardEnemy(500, 75, "a", 0)
-	E[2] = getStandardEnemy(1000, 350, "b", 0)
-	E[3] = getStandardEnemy(1300, 80, "c", 0)
-	E[4] = getStandardEnemy(1600, 180, "d", 0)
-	E[5] = getStandardEnemy(1900, 100, "e", 0)
-	E[6] = getStandardEnemy(2100, 300, "f", 0)
-	E[7] = getStandardEnemy(2400, 275, "g", 0)
+	E[1] = getStandardEnemy(1100, 75, "a", -1)
+	E[2] = getStandardEnemy(1000, 350, "b", -1)
+	E[3] = getStandardEnemy(1300, 80, "c", -1)
+	E[4] = getStandardEnemy(1600, 180, "d", -1)
+	E[5] = getStandardEnemy(1900, 100, "e", -1)
+	E[6] = getStandardEnemy(2100, 300, "f", -1)
+	E[7] = getStandardEnemy(2400, 275, "g", -1)
 
 	return E
 end
