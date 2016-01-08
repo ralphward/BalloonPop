6cd3a31df12ac95f7ab995971c7ff622fc9adb23
Ralph Ward <ralph.ward@gmail.com>
2016-01-08 22:14:13 +1100

    set some color schemes on main

--- menu/menu.lua
+++ menu/menu.lua
@@ -53,74 +53,86 @@ function scene:create( event )
     -- setup a page background, really not that important though composer
     -- crashes out if there isn't a display object in the view.
     --
+    local purple = {40/255, 20/255, 63/255}
+    local blue_green = {17/255, 205/255, 197/255}
+    local blue = {74/255, 144/255, 226/255}
+    local pink = {252/255, 45/255, 121/255}
+    local yellow = {252/255, 182/255, 53/255}
+    local green = {85/255, 200/255, 85/255}
+
     local background = display.newRect( 0, 0, 570, 360 )
     background.x = display.contentCenterX
     background.y = display.contentCenterY
+    background.fill = purple
     sceneGroup:insert( background )
-
-    local title = display.newText("Balloon pop", 100, 32, native.systemFontBold, 32 )
-    title.x = display.contentCenterX
-    title.y = 40
-    title:setFillColor( 0 )
-    sceneGroup:insert( title )
 
     -- Create the widget
     local playButton = widget.newButton({
         id = "button1",
+        shape = "circle",
+        fillColor = {default=blue_green, over=blue_green},
         label = "Play",
         width = 100,
         height = 32,
         onEvent = handlePlayButtonEvent
     })
-    playButton.x = display.contentCenterX / 2
-    playButton.y = display.contentCenterY - 60
+    playButton.x = display.contentCenterX
+    playButton.y = display.contentCenterY
     sceneGroup:insert( playButton )
 
     local surviveButton = widget.newButton({
         id = "button1",
         label = "Survival",
+        shape = "circle",
+        fillColor = {default=pink, over=pink},
         width = 100,
         height = 32,
         onEvent = handleSurvivalButtonEvent
     })
-    surviveButton.x = display.contentCenterX * 1.5
-    surviveButton.y = display.contentCenterY - 60
+    surviveButton.x = display.contentCenterX / 2
+    surviveButton.y = display.contentCenterY / 2
     sceneGroup:insert( surviveButton )
 
     -- Create the widget
     local settingsButton = widget.newButton({
         id = "button2",
         label = "Settings",
+        shape = "circle",
+        fillColor = {default=blue, over=blue},
         width = 100,
         height = 32,
         onEvent = handleSettingsButtonEvent
     })
     settingsButton.x = display.contentCenterX / 2
-    settingsButton.y = display.contentCenterY
+    settingsButton.y = display.contentCenterY * 1.5
     sceneGroup:insert( settingsButton )
 
     -- Create the widget
     local helpButton = widget.newButton({
         id = "button3",
         label = "Help",
+        shape = "circle",
+        fillColor = {default=yellow, over=yellow},
         width = 100,
         height = 32,
         onEvent = handleHelpButtonEvent
     })
     helpButton.x = display.contentCenterX * 1.5
-    helpButton.y = display.contentCenterY
+    helpButton.y = display.contentCenterY / 2
     sceneGroup:insert( helpButton )
 
     -- Create the widget
     local creditsButton = widget.newButton({
         id = "button4",
         label = "Credits",
+        shape = "circle",
+        fillColor = {default=green, over=green},
         width = 100,
         height = 32,
         onEvent = handleCreditsButtonEvent
     })
-    creditsButton.x = display.contentCenterX / 2
-    creditsButton.y = display.contentCenterY + 60
+    creditsButton.x = display.contentCenterX * 1.5
+    creditsButton.y = display.contentCenterY * 1.5
     sceneGroup:insert( creditsButton )
 
 end
